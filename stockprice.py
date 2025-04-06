84c7907004ac6a2553902a71e2b1d2f88cb2c176
90polarbear <100282618+90polarbear@users.noreply.github.com>
2025-04-06 10:20:31 +0800

    Update stockprice.py

--- stockprice.py
+++ stockprice.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 from bs4 import BeautifulSoup
 import Imgur
+import matplotlib
 from matplotlib.font_manager import FontProperties # 設定字體
 chinese_font = matplotlib.font_manager.FontProperties(fname='msjh.ttf') # 引入同個資料夾下支援中文字檔
 
